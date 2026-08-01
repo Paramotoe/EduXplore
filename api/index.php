508f8b09653a5658eaa99008928873c4bbdb3560
Paramotoe <kamuikanna25@gmil.com>
2026-08-01 22:00:44 +0700

    Unmask real Laravel error in api/index.php

--- api/index.php
+++ api/index.php
@@ -1,1 +1,8 @@
-PHP
+';
+    echo '💥 ERROR ASLI TERDETEKSI:';
+    echo 'Penyebab (Class): ' . get_class($e) . '';
+    echo 'Pesan Error: ' . $e->getMessage() . '';
+    echo 'File: ' . $e->getFile() . ' (Baris ' . $e->getLine() . ')';
+    echo '';
+    exit(1);
+}
